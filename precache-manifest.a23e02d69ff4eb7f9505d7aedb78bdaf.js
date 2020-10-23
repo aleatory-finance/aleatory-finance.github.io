e199669f2c3185286fb2fce0c1272fbdf698ebc0
aleatory-finance <73224715+aleatory-finance@users.noreply.github.com>
2020-10-23 07:52:09 +0300

    Add files via upload

---
+++ precache-manifest.a23e02d69ff4eb7f9505d7aedb78bdaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a16ee1017fabde42e379",
+    "url": "/static/css/main.e8543a9a.chunk.css"
+  },
+  {
+    "revision": "a16ee1017fabde42e379",
+    "url": "/static/js/main.a16ee101.chunk.js"
+  },
+  {
+    "revision": "b9122ea58857428bfd97",
+    "url": "/static/css/1.6582b6c6.chunk.css"
+  },
+  {
+    "revision": "b9122ea58857428bfd97",
+    "url": "/static/js/1.b9122ea5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f00ad8fd3e8464cceeb4bf4b08e143a7",
+    "url": "/static/media/dice.f00ad8fd.png"
+  },
+  {
+    "revision": "c9a8bb233dac3a387209634c3d9240f9",
+    "url": "/index.html"
+  }
+];
